4af1e8c1b56e271ba37d03ab806a4685f5cbe2e2
Piyushsharma218 <sharmasatyam524@gmail.com>
2026-01-06 23:43:28 +0530

    added all basic files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import recipeDetailView from "./components/recipeDetailView";
+import SearchView from "./components/SearchView";
+import Cuisine from "./components/Cuisine";
+import HomeView from "./components/HomeView";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="min-h-screen bg-gray-950 font-sans text-gray-100">
+          <Navbar/> 
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
